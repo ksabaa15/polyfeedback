9a15d144c57180f63354018018325f0ec256191e
Karim Sabaa <karim.sabaa@epfl.ch>
2022-07-30 22:01:40 +0200

    feature: add database schemas #4

---
+++ backend/src/models/course.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const schema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: [true, 'Course code is required.'],
+    unique: true,
+    minLength: [6, 'Course code too short.'],
+    maxLength: [12, 'Course code too long.'],
+    trim: true,
+  },
+  title: {
+    type: String,
+    required: [true, 'Course title is required.'],
+    minLength: [4, 'Course title too short.'],
+    maxLength: [148, 'Course title too long'],
+    trim: true,
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+});
+
+const joiSchema = Joi.object({
+  code: Joi.string().required().min(6).max(12),
+  title: Joi.string().required().min(4).max(148),
+  reviews: Joi.array().items(
+    Joi.object({
+      review: Joi.objectId(),
+    })
+  ),
+});
+function validate(review) {
+  return joiSchema.validate(review);
+}
+
+module.exports.Course = mongoose.model('Course', schema);
+module.exports.validate = validate;
---
+++ backend/src/models/review.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const schema = new mongoose.Schema({
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+    minLength: [50, 'Review text is too short.'],
+    maxLength: [1000, 'Review text is too long.'],
+    trim: true,
+  },
+});
+
+function validate(review) {
+  const joiSchema = Joi.object({
+    course: Joi.object({ course: Joi.objectId() }).required(),
+    user: Joi.object({ course: Joi.objectId() }).required(),
+    text: Joi.string().required().min(50).max(1000),
+  });
+  return joiSchema.validate(review);
+}
+
+module.exports.Review = mongoose.model('Review', schema);
+module.exports.validate = validate;
