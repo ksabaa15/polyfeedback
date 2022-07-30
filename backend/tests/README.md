956aa6c02b9c43b3a4c0e5b259ee02ef097c8cc4
Karim Sabaa <karim.sabaa@epfl.ch>
2022-07-30 22:02:57 +0200

    test: add tests of database schemas #4

---
+++ backend/tests/README.md
@@ -0,0 +1,29 @@
+# Tests
+
+Test cases based on [Jest](https://jestjs.io/), a javascript testing framework.
+
+## Run
+
+To run the tests, please execute the following command:
+
+```shell
+npm test
+```
+
+## Dependencies
+
+### NPM Dependencies
+
+```shell
+npm install
+```
+
+### Additional Dependencies
+
+* #### MongoDB 4.4
+
+    Please refere to the official [MongoDB 4.4 installation guide](https://www.mongodb.com/docs/v4.4/installation/).
+
+## References
+
+For any additional information, please refer to the documentation of [Jest](https://jestjs.io/).
---
+++ backend/tests/integration/src/models/course.test.js
@@ -0,0 +1,68 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const { Course, validate } = require('../../../../src/models/course');
+
+describe('Course', () => {
+  let courseObj = {};
+  beforeAll(async () => {
+    await mongoose.connect(config.database);
+  });
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  beforeEach(async () => {
+    await Course.deleteMany({});
+    courseObj = {
+      code: 'PHYS-118',
+      title: 'Building physics',
+      reviews: [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()],
+    };
+  });
+
+  it('should add course to database successfully', async () => {
+    const course = new Course(courseObj);
+    await expect(course.save()).resolves.toMatchObject(courseObj);
+    expect(validate(courseObj).error).toBeUndefined();
+  });
+  it('should fail to add course to database because missing required property `code` ', async () => {
+    delete courseObj.code;
+    const course = new Course(courseObj);
+    await expect(course.save()).rejects.toThrowError(/.*code.*/);
+    expect(validate(courseObj).error).not.toBeUndefined();
+  });
+  it('should fail to add course to database because missing required property `title` ', async () => {
+    delete courseObj.title;
+    const course = new Course(courseObj);
+    await expect(course.save()).rejects.toThrowError(/.*title.*/);
+    expect(validate(courseObj).error).not.toBeUndefined();
+  });
+  it('should successfully add course to database and replace missing property `reviews` with an empty array ', async () => {
+    delete courseObj.reviews;
+    const course = new Course(courseObj);
+    courseObj.reviews = [];
+    await expect(course.save()).resolves.toMatchObject(courseObj);
+    expect(validate(courseObj).error).toBeUndefined();
+  });
+
+  it('should fail to add course to database because wrong type of property `code`', async () => {
+    courseObj.code = 123;
+    const course = new Course(courseObj);
+    await expect(course.save()).rejects.toThrowError(/.*code.*/);
+    expect(validate(courseObj).error).not.toBeUndefined();
+  });
+  it('should fail to add course to database because wrong type of property `title`', async () => {
+    courseObj.title = 123;
+    const course = new Course(courseObj);
+    await expect(course.save()).rejects.toThrowError(/.*title.*/);
+    expect(validate(courseObj).error).not.toBeUndefined();
+  });
+  it('should successfully add course to database because wrong type of property `reviews`', async () => {
+    courseObj.reviews = [123];
+    console.log(courseObj);
+    const course = new Course(courseObj);
+    courseObj.reviews = [];
+    await expect(course.save()).rejects.toThrowError(/.*reviews.*/);
+    expect(validate(courseObj).error).toBeUndefined();
+  });
+});
